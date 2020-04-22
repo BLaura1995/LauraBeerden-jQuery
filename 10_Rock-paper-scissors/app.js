0c2292788c2e196fd4b82b6435ac2c3002342250
Laura Beerden <60176579+BLaura1995@users.noreply.github.com>
2020-04-22 10:18:02 +0200

    update

--- 10_Rock-paper-scissors/app.js
+++
@@ -1,5 +0,0 @@
-// Bron: https://www.youtube.com/watch?v=qWPtKtYEsN4&feature=youtu.be
-$('.intro button').on('click', function() {
-    $('.intro').fadeOut('slow');
-    $('.match').fadeIn('slow');
-})
