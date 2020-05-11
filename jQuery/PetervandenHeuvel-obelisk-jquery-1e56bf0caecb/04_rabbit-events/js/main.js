f4ee65027f2a6c4cca1218d83a1c2c2e1f7f02fb
Laura Beerden <60176579+BLaura1995@users.noreply.github.com>
2020-05-11 10:15:26 +0200

    update

---
+++ jQuery/PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/04_rabbit-events/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // 1 Event that listens for click on img
+    // 2 Make the clicked image disappear with: $(this)...
+    // 3 Inside function:  Make clicked rabbit disapper, maybe use fadeOut() method, look on jquery.com: http://api.jquery.com/fadeout/
+    $('img').on('click', function() {
+        $(this).fadeOut('slow');
+    })
+}); // End of document.ready functie.
---
+++ jQuery/PetervandenHeuvel-obelisk-jquery-1e56bf0caecb/06_Input/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Your Code here
+    $('button#btn-hello').on('click', function() {
+        alert('hello!');
+    });
+    $('input:submit').on('click', function() {
+
+    })
+}); // End of document.ready functie.
